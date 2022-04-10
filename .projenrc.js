909b9d35805f3f82f6a8d376dc7eaba06af25331
epolon <epolon@amazon.com>
2022-04-10 12:12:19 +0300

    chore: introduce node-backpack

---
+++ .projenrc.js
@@ -0,0 +1,41 @@
+const { typescript } = require('projen');
+const project = new typescript.TypeScriptProject({
+  name: 'node-backpack',
+  defaultReleaseBranch: 'main',
+  releaseToNpm: false,
+  devDeps: [
+    '@types/madge',
+    '@types/license-checker',
+    '@types/fs-extra',
+  ],
+  deps: [
+    'esbuild',
+    'madge',
+    'license-checker',
+    'yargs',
+    'fs-extra',
+    'shlex',
+  ],
+  bin: {
+    'node-backpack': 'bin/node-backpack',
+  },
+  autoApproveOptions: {
+    allowedUsernames: ['cdklabs-automation'],
+    secret: 'GITHUB_TOKEN',
+  },
+  autoApproveUpgrades: true,
+});
+
+project.gitignore.exclude('.vscode/');
+
+// required for esbuild > v0.14.32
+// see https://github.com/evanw/esbuild/pull/2155
+// see https://stackoverflow.com/questions/56906718/error-ts2304-cannot-find-name-webassembly
+project.tsconfig.compilerOptions.lib.push('dom');
+
+// needed for CLI tests to run
+project.testTask.prependSpawn(project.compileTask);
+
+project.addFields({ private: true });
+
+project.synth();
