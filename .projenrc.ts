2d633009d7a648aa2d33a0273b0e8d38405bfdd4
Momo Kornher <mail@moritzkornher.de>
2023-06-13 16:10:52 +0100

    chore: .projenrc.ts

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,6 +1,7 @@
-const { typescript } = require('projen');
+import { typescript } from 'projen';
 const project = new typescript.TypeScriptProject({
   name: 'node-backpack',
+  projenrcTs: true,
   defaultReleaseBranch: 'main',
   releaseToNpm: true,
   devDeps: [
@@ -31,7 +32,7 @@ project.gitignore.exclude('.vscode/');
 // required for esbuild > v0.14.32
 // see https://github.com/evanw/esbuild/pull/2155
 // see https://stackoverflow.com/questions/56906718/error-ts2304-cannot-find-name-webassembly
-project.tsconfig.compilerOptions.lib.push('dom');
+project.tsconfig?.compilerOptions.lib?.push('dom');
 
 // needed for CLI tests to run
 project.testTask.prependSpawn(project.compileTask);
