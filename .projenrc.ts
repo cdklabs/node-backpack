ee1ce117c98b77cd703cc7e7f84f8e5863b7b0ad
SankyRed <sankyred@amazon.com>
2024-03-13 16:18:11 -0500

    fix TS compilerOptions optional issue

--- .projenrc.ts
+++ .projenrc.ts
@@ -31,7 +31,7 @@ project.gitignore.exclude('.vscode/');
 // required for esbuild > v0.14.32
 // see https://github.com/evanw/esbuild/pull/2155
 // see https://stackoverflow.com/questions/56906718/error-ts2304-cannot-find-name-webassembly
-project.tsconfig?.compilerOptions.lib?.push('dom');
+project.tsconfig?.compilerOptions?.lib?.push('dom');
 
 // needed for CLI tests to run
 project.testTask.prependSpawn(project.compileTask);
